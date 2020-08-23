2d0e4de5762f82a9182690766a451c59d93d3ff5
MuhammadNurHidayat07092000 <yyt08536@gmail.com>
2020-08-23 12:17:15 +0800

    jquery ajax

---
+++ p20/js/scrip.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    //hilangkan tombol cari
+    $('#tombol-cari').hide();
+
+    //event ketika keyword ditulis
+    $('#keyword').on('keyup', function () {
+
+        //munculkan icon loading
+        $('.load').show();
+
+        // $('#container').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+
+        $.get('ajax/mahasiswa.php?keyword=' + $('#keyword').val(), function (data) {
+
+            $('#container').html(data);
+            $('.load').hide();
+
+
+        });
+    });
+});
